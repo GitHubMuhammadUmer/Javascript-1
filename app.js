9cb9c541e16478830dbc0be9def725406f2b9385
Muhammad Umer <140950473+GitHubMuhammadUmer@users.noreply.github.com>
2023-10-14 02:40:46 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,129 @@
+//  ___________ Alert ________________
+
+//     -------Question No.1--------
+// alert("Wellcome User");
+
+//     -------Question No.2--------
+// alert("Error! Please Enter A Valid Password");
+
+//     -------Question No.3--------
+// alert("Welcome to Js Land... \n Happay Coding!");
+
+//     -------Question No.4 (a)--------
+// alert("Welcome to Js Land");
+
+//     -------Question No.4 (b)--------
+// alert(" Happay Coding! \n Prevent this page form creating assitional dailogs");
+
+//     -------Question No.5--------
+// alert("Hello... I can run JS through my web browser's console")
+
+
+//  _____________ Variables For String  ________________
+
+//     -------Question No.1--------
+// var username;
+
+//     -------Question No.2--------
+// var myName = "Full Name";
+
+//     -------Question No.3--------
+
+// var message = "“Hello World";
+// alert("Hello World")
+
+//     -------Question No.4--------
+// var name = "Jhone Doe";
+// alert(name)
+// var age = 15;
+// alert(age)
+// var education = "Certified Moblie Application Development"
+// alert(education)
+
+//     -------Question No.5--------
+// var name = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(name);
+
+//     -------Question No.6--------
+// var email = "example@example.com";
+// alert("My email address is " + email;
+
+//     -------Question No.7--------
+// var book = "A smarter way to learn JavaScript";
+// alert(book);
+
+
+//     -------Question No.8--------
+
+// var word = "Yah! I can Write HTML content through JavaScript";
+// document.write(word);
+
+//     -------Question No.9--------
+// var word = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(word);
+
+
+//  _____________ Variables For Number  ________________
+//     -------Question No.1--------
+
+// var word = "I am"
+// var age = 15;
+// var name = "years old"
+// alert(word + " " + age + " " + name);
+
+
+//     -------Question No.2--------
+// var word = "You have visited this site";
+// var num = "14";
+// var tim = "times";
+// alert(word + " " + num + " " + tim);
+
+//     -------Question No.3--------
+// var birthYear ="My brith year is 1990  \n " ;
+// var word = "Date type of my declared variable is number";
+// document.write(birthYear); 
+// document.write(word);
+
+
+
+//     -------Question No.4--------
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store");
+
+
+
+
+// _____________ VARIABLE NAMES: LEGAL & ILLEGAL ________________ 
+
+//     -------Question No.1--------
+// var word = "Pakistan";
+// var num = 10;
+// var boolen = true;
+
+//     -------Question No.2--------
+ // LEGAL 
+// var word = "Pakistan";
+// var full_name ="Muhammad Umer";
+// var lastName = "Iqbal";
+// var Word = "Saylani";
+// var NumNo = 15;
+// var boolen1 = fale;
+// var boll2en = ture;
+
+//  ILLEGAL
+
+// var 1word = "Pakistan"
+// var 1word = "Pakistan";
+// var 3full_name ="Muhammad Umer";
+// var 5lastName = "Iqbal";
+// var 6Word = "Saylani";
+// var 7NumNo = 15;
+// var 8boolen1 = fale;
+// var 9boll2en = ture;
+ 
+
+
+//     -------Question No.3--------
+// var word = "A heading stating “Rules for naming JS variables” \n Variable names can only contain ______, ______, \n ______ and ______. \n For example $my_1stVariable \n Variables must begin with a ______, ______ or \n  _____. For example $name, _name or name \n Variable names are case _________ \n Variable names should not be JS _________";
+// document.write(word);
+
+
